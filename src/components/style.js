22be898009d46bdb80f8ffb6e5d263409a5cee12
maxhudson <max@henrybuilt.com>
2021-04-30 16:18:08 -0600

    arrow size

--- src/components/style.js
+++ src/components/style.js
@@ -6,7 +6,7 @@ export const STEP_NUMBER_DIAMETER = STEP_NUMBER_RADIUS * 2;
 export const ZINDEX = 100;
 export const MARGIN = 13;
 export const OFFSET_WIDTH = 4;
-export const ARROW_SIZE = 6;
+export const ARROW_SIZE = 10;
 
 export default StyleSheet.create({
   container: {
