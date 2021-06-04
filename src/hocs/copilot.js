c3259b0cc6ef03c29694c66f6f54f16f5db30c5d
maxhudson <max@henrybuilt.com>
2021-06-04 10:33:31 -0700

    passing stop

--- src/hocs/copilot.js
+++ src/hocs/copilot.js
@@ -191,6 +191,7 @@ const copilot = ({
             <WrappedComponent
               {...this.props}
               start={this.start}
+              stop={this.stop}
               currentStep={this.state.currentStep}
               visible={this.state.visible}
               copilotEvents={this.eventEmitter}
